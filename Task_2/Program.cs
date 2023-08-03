9f994ba761128989505f21f943aa2ee0ef72fef6
Daredevil <aliyor.0276@gmail.com>
2023-08-03 22:02:49 +0500

    find max

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a < b)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(b);
+}
+else 
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(a);
+}
