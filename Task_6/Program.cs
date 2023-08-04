26ebfc9620571721cd294028e3d634a8b07af614
Daredevil <aliyor.0276@gmail.com>
2023-08-04 22:05:26 +0500

    if even

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+int even = a % 2;
+if (even == 0)
+    {
+        Console.WriteLine("True");
+    }
+else
+{
+    Console.WriteLine("False");
+}
