1d987e18c6d84b5b5f174615f8993c473eaeaba1
Daredevil <aliyor.0276@gmail.com>
2023-08-04 22:15:07 +0500

    all even

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+        if (i %2 == 0)
+    {
+            Console.Write(i + ", ");
+    }    
+    i=i+1;
+}
