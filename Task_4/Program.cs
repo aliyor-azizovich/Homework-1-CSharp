9a8bffa52fbcf1cac5ca987ee9140f95666e8014
Daredevil <aliyor.0276@gmail.com>
2023-08-04 21:50:54 +0500

    max 2

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ( a > b && a > c)
+{
+    max = a;
+}
+else if (b > c)
+    {    
+        max = b;
+    }
+    else 
+    {
+        max = c;
+    }
+Console.Write("Максимальное число: " + max);
